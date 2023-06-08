39745170e1b09b598982a0fc563b4c763875b191
chiarapascucci-gds <chiara.pascucci@digital.cabinet-office.gov.uk>
2023-06-08 14:12:10 +0100

    rest style API

--- consumer/src/api.js
+++ consumer/src/api.js
@@ -36,7 +36,7 @@ export class API {
     }
 
     async getProduct(id) {
-        return axios.get(this.withPath("/product/" + id), {
+        return axios.get(this.withPath("/products/" + id), {
             headers: {
                 "Authorization": this.generateAuthToken()
             }
